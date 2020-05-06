2d0dfb550bdaac801cbce262a3c8943e8f84e47b
Thomas G <nbtmentertainement@gmail.com>
2020-05-06 11:08:45 +0200

    Initial Commit

---
+++ Xcode test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Xcode test
+//
+//  Created by thomas groux on 06/05/2020.
+//  Copyright © 2020 thomas groux. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
